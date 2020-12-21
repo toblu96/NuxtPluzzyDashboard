e8069f7fcd16d8a0ec4ecabb256d28e33482d9ee
Tobias Blum <tobias.blum@besonet.ch>
2020-12-21 22:28:18 +0100

    New Activity Feed

---
+++ components/project/ActivityFeed.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="lg:w-80">
+    <div class="pt-6 pb-2">
+      <h2 class="text-sm font-semibold">Activity</h2>
+    </div>
+    <div>
+      <ul class="divide-y divide-gray-200">
+        <li v-for="commit in commits.slice(0, 5)" :key="commit.id" class="py-4">
+          <div class="flex space-x-3">
+            <img class="h-6 w-6 rounded-full" :src="commit.avatar_url" alt="" />
+            <div class="flex-1 space-y-1">
+              <div class="flex items-center justify-between">
+                <h3 class="text-sm font-medium">
+                  {{
+                    $auth.user.email == commit.author_email
+                      ? "You"
+                      : commit.author_email
+                  }}
+                </h3>
+                <p class="text-sm text-gray-500">
+                  {{ $moment(commit.committed_date).fromNow(true) }}
+                </p>
+              </div>
+              <p class="text-sm text-gray-500">
+                {{ commit.message }} ({{ commit.short_id }} in master)
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <!-- More items... -->
+      </ul>
+      <div class="py-4 text-sm border-t border-gray-200">
+        <a href="#" class="text-orange-600 font-semibold hover:text-orange-900"
+          >View all activity <span aria-hidden="true">&rarr;</span></a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      commits: [],
+    };
+  },
+  async mounted() {
+    await this.getCommits();
+  },
+  methods: {
+    async getCommits() {
+      // get Project Files
+      const responses = await Promise.all([this.$gitApi.getProjectCommits()]);
+      const badResponse = responses.find((response) => !response.ok);
+      if (badResponse) {
+        $nuxt.error({
+          statusCode: badResponse.status,
+          message: badResponse.statusText,
+        });
+      }
+
+      const commits = responses[0].json;
+      //   add avatar url
+      await Promise.all(
+        commits.map(async (commit, index) => {
+          const avatarUrl = await this.$gitApi.getUserAvatar(
+            commit.author_email
+          );
+          commits[index].avatar_url = avatarUrl.json.avatar_url;
+        })
+      );
+
+      this.commits = commits;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
