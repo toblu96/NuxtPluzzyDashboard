e4619d6c766128ec9c168e4bd4acb20a1cce2032
Tobias Blum <tobias.blum@besonet.ch>
2021-01-08 18:20:51 +0100

    New project welcome screen

---
+++ pages/project/files.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <layout-three-col>
+      <template slot="sidebar">
+        <project-sidebar
+          @showGraphicsModal="$refs.showGraphicsModal.show()"
+          @showConfigModal="$refs.showConfigModal.show()"
+        />
+      </template>
+      <template slot="main">
+        <projects-list />
+      </template>
+      <template slot="right">
+        <activity-feed />
+      </template>
+    </layout-three-col>
+    <modal-upload-graphics ref="showGraphicsModal" />
+    <modal-upload-config ref="showConfigModal" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "application",
+};
+</script>
+
+<style>
+</style>
--- pages/project/index.vue
+++ pages/project/index.vue
@@ -1,22 +1,152 @@
 <template>
-  <div>
-    <layout-three-col>
-      <template slot="sidebar">
-        <project-sidebar
-          @showGraphicsModal="$refs.showGraphicsModal.show()"
-          @showConfigModal="$refs.showConfigModal.show()"
-        />
-      </template>
-      <template slot="main">
-        <projects-list />
-      </template>
-      <template slot="right">
-        <activity-feed />
-      </template>
-    </layout-three-col>
-    <modal-upload-graphics ref="showGraphicsModal" />
-    <modal-upload-config ref="showConfigModal" />
-  </div>
+  <main class="max-w-7xl mx-auto pb-10 lg:py-12 lg:px-8">
+    <!-- Starting steps list -->
+    <div class="bg-gray-100 p-4 rounded-md">
+      <ul
+        class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      >
+        <!-- Load your connection configs -->
+        <li
+          class="col-span-1 flex flex-col space-y-8 text-center bg-gradient-to-tr from-indigo-700 to-pink-500 rounded-md shadow p-4"
+        >
+          <!-- Heading -->
+          <div class="flex flex-row items-center space-x-4">
+            <span
+              class="inline-flex justify-center items-center h-6 w-6 rounded-full text-sm font-medium bg-pink-100 text-pink-800"
+            >
+              <p>1</p>
+            </span>
+            <h3 class="text-pink-100 text-sm font-medium">
+              Configure a PLC Connection
+            </h3>
+          </div>
+          <!-- Illustration -->
+          <!-- https://icons8.com/illustrations/style--pixeltrue -->
+          <div class="flex-1 flex flex-col">
+            <img
+              class="w-full flex-shrink-0 my-auto"
+              src="~/assets/images/configure-system.svg"
+            />
+          </div>
+          <!-- Footer -->
+          <div>
+            <NuxtLink
+              to="#"
+              type="button"
+              class="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-base font-medium text-pink-800 bg-pink-200 hover:bg-pink-300 focus:outline-none sm:text-sm"
+            >
+              Load MSR List
+            </NuxtLink>
+          </div>
+        </li>
+
+        <!-- Load dashboards -->
+        <li
+          class="col-span-1 flex flex-col space-y-8 text-center bg-gray-50 rounded-md shadow p-4"
+        >
+          <!-- Heading -->
+          <div class="flex flex-row items-center space-x-4">
+            <span
+              class="inline-flex justify-center items-center h-6 w-6 rounded-full text-sm font-medium bg-pink-100 text-pink-800"
+            >
+              <p>2</p>
+            </span>
+            <h3 class="text-gray-900 text-sm font-medium">
+              Create a new Dashboard
+            </h3>
+          </div>
+          <!-- Illustration -->
+          <!-- https://icons8.com/illustrations/style--pixeltrue -->
+          <div class="flex-1 flex flex-col">
+            <img
+              class="w-full flex-shrink-0 my-auto"
+              src="~/assets/images/design-dashboard.svg"
+            />
+          </div>
+          <!-- Footer -->
+          <div>
+            <NuxtLink
+              to="#"
+              type="button"
+              class="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-base font-medium text-pink-800 bg-pink-200 hover:bg-pink-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 sm:text-sm"
+            >
+              Import Dashboard
+            </NuxtLink>
+          </div>
+        </li>
+
+        <!-- Explore Data -->
+        <li
+          class="col-span-1 flex flex-col space-y-8 text-center bg-gray-50 rounded-md shadow p-4"
+        >
+          <!-- Heading -->
+          <div class="flex flex-row items-center space-x-4">
+            <span
+              class="inline-flex justify-center items-center h-6 w-6 rounded-full text-sm font-medium bg-pink-100 text-pink-800"
+            >
+              <p>3</p>
+            </span>
+            <h3 class="text-gray-900 text-sm font-medium">
+              Explore Data Connections
+            </h3>
+          </div>
+          <!-- Illustration -->
+          <!-- https://icons8.com/illustrations/style--pixeltrue -->
+          <div class="flex-1 flex flex-col">
+            <img
+              class="w-full flex-shrink-0 my-auto"
+              src="~/assets/images/explore-data.svg"
+            />
+          </div>
+          <!-- Footer -->
+          <div>
+            <NuxtLink
+              to="#"
+              type="button"
+              class="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-base font-medium text-pink-800 bg-pink-200 hover:bg-pink-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 sm:text-sm"
+            >
+              Explore Data
+            </NuxtLink>
+          </div>
+        </li>
+
+        <!-- Inspect system -->
+        <li
+          class="col-span-1 flex flex-col space-y-8 text-center bg-gray-50 rounded-md shadow p-4"
+        >
+          <!-- Heading -->
+          <div class="flex flex-row items-center space-x-4">
+            <span
+              class="inline-flex justify-center items-center h-6 w-6 rounded-full text-sm font-medium bg-pink-100 text-pink-800"
+            >
+              <p>4</p>
+            </span>
+            <h3 class="text-gray-900 text-sm font-medium">
+              Inspect your System
+            </h3>
+          </div>
+          <!-- Illustration -->
+          <!-- https://icons8.com/illustrations/style--pixeltrue -->
+          <div class="flex-1 flex flex-col">
+            <img
+              class="w-full flex-shrink-0 my-auto"
+              src="~/assets/images/data-analyse.svg"
+            />
+          </div>
+          <!-- Footer -->
+          <div>
+            <NuxtLink
+              to="#"
+              type="button"
+              class="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-base font-medium text-pink-800 bg-pink-200 hover:bg-pink-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 sm:text-sm"
+            >
+              System Status
+            </NuxtLink>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script>
